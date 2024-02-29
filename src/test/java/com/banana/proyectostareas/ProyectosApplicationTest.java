d30fe4aff01fcc1c93f55f4fdecbb1fdb0b39939
myGitJuan <jarellano.aguirre@gmail.com>
2024-02-29 23:51:25 +0100

    montar base

---
+++ src/test/java/com/banana/proyectostareas/ProyectosApplicationTest.java
@@ -0,0 +1,12 @@
+package com.banana.proyectostareas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProyectosApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
---
+++ src/test/resources/testing_clean.sql
@@ -0,0 +1,2 @@
+TRUNCATE  TABLE proyectos;
+TRUNCATE  TABLE tareas;
