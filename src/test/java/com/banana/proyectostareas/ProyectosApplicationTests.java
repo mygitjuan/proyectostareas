782bb25bb803f8a34cdd341cc5928065af63e507
myGitJuan <jarellano.aguirre@gmail.com>
2024-02-29 22:51:30 +0100

    carga proyecto inicial

---
+++ src/test/java/com/banana/proyectostareas/ProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banana.proyectostareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
