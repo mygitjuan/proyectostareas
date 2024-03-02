bfbbb46bd65ae911439d733c8a6b38a143d5dfb0
myGitJuan <jarellano.aguirre@gmail.com>
2024-03-02 20:52:55 +0100

    entity y test de humo pasado

--- src/main/java/com/banana/proyectostareas/model/Proyecto.java
+++ src/main/java/com/banana/proyectostareas/model/Proyecto.java
@@ -1,16 +1,38 @@
 package com.banana.proyectostareas.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Table(name = "proyecto")
+@Schema(name = "proyecto", description = "Modelo proyecto")
 public class Proyecto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    @Min(1)
+    @Schema(name = "ProyectoID", example = "1", required = false)
     private Long id;
+    @Column(name="nombre")
+    @Size(min = 3, max = 20)
+    @Schema(name = "nombre", example = "UML", required = true)
     private String nombre;
+    @Column(name="fecha_creacion")
+    @DateTimeFormat
+    @NotNull
+    @Schema(name = "fechaCreacion", example = "2024-01-31", required = true)
     private LocalDate fechaCreacion;
 }
--- src/main/java/com/banana/proyectostareas/model/Tarea.java
+++ src/main/java/com/banana/proyectostareas/model/Tarea.java
@@ -1,19 +1,47 @@
 package com.banana.proyectostareas.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Table(name = "tarea")
+@Schema(name = "tarea", description = "Modelo tarea")
 public class Tarea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    @Min(1)
+    @Schema(name = "TareaID", example = "1", required = false)
     private Long id;
+    @Column(name="descripcion")
+    @Size(min = 3, max = 20)
+    @Schema(name = "descripcion", example = "UML", required = true)
     private String descripcion;
+    @Column(name="fecha_limite")
+    @DateTimeFormat
+    @NotNull
+    @Schema(name = "fechaLimite", example = "2024-01-31", required = true)
     private LocalDate fechaLimite;
+    @Column(name="orden")
+    @Min(1)
+    @Schema(name = "orden", example = "1", required = true)
     private Integer orden;
+    @Column(name="completada")
+    @NotNull
+    @Schema(name = "completada", example = "", required = true)
     private boolean completada;
 
 }
