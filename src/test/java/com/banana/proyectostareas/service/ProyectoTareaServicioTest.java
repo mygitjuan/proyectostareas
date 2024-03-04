9e3e8b9bed83d1232a5b6e0898a875a817a801a0
myGitJuan <jarellano.aguirre@gmail.com>
2024-03-04 16:54:17 +0100

    crear servicios

--- src/test/java/com/banana/proyectostareas/service/ProyectoTareaServicioTest.java
+++ src/test/java/com/banana/proyectostareas/service/ProyectoTareaServicioTest.java
@@ -4,10 +4,15 @@ import com.banana.proyectostareas.exception.ProyectoNotfoundException;
 import com.banana.proyectostareas.model.Proyecto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -17,16 +22,21 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
-@DataJpaTest
 @ComponentScan(basePackages = {"com.banana.proyectostareas.service","com.banana.proyectostareas.persistence"})
+@DataJpaTest
 @AutoConfigureTestEntityManager
 class ProyectoTareaServicioTest {
     @Autowired
-    ProyectoTareaService proyectoTareaService;
+    private ProyectoTareaService proyectoTareaService;
+
+    private static final Logger logger = LoggerFactory.getLogger(ProyectoTareaServicioTest.class);
+
+    @Autowired
+    private TestEntityManager em;
 
     @Test
     void obtenerProyectosValido() {
-        proyectoTareaService.obtenerProyectos();
+
         List<Proyecto> proyectoList = null;
 
         try {
