a49df7a63c670e957b4e362dc5a193f9cc87994b
myGitJuan <jarellano.aguirre@gmail.com>
2024-03-04 15:43:05 +0100

    crear servicios

--- src/main/java/com/banana/proyectostareas/service/IServicioProyecto.java
+++
@@ -1,7 +0,0 @@
-package com.banana.proyectostareas.service;
-
-import com.banana.proyectostareas.model.Proyecto;
-
-public interface IServicioProyecto {
-    public Proyecto crearProyecto(Proyecto proyecto, Long indTarea);
-}
---
+++ src/main/java/com/banana/proyectostareas/service/ProyectoTareaServicio.java
@@ -0,0 +1,36 @@
+package com.banana.proyectostareas.service;
+
+import com.banana.proyectostareas.exception.ProyectoNotfoundException;
+import com.banana.proyectostareas.exception.TareaNotfoundException;
+import com.banana.proyectostareas.model.Proyecto;
+import com.banana.proyectostareas.model.Tarea;
+
+import java.util.List;
+
+public class ProyectoTareaServicio implements ProyectoTareaService{
+
+    @Override
+    public Proyecto crearProyecto(Proyecto proyecto) throws RuntimeException {
+        return null;
+    }
+
+    @Override
+    public Proyecto anadeTareaAProyecto(Long idProyecto, Tarea tarea) throws ProyectoNotfoundException, RuntimeException {
+        return null;
+    }
+
+    @Override
+    public List<Proyecto> obtenerProyectos() throws ProyectoNotfoundException, RuntimeException {
+        return null;
+    }
+
+    @Override
+    public List<Tarea> obtenerTareasDelProyecto(Long idProyecto) throws ProyectoNotfoundException, RuntimeException {
+        return null;
+    }
+
+    @Override
+    public Tarea marcarTareaHecha(Long idTarea) throws TareaNotfoundException, RuntimeException {
+        return null;
+    }
+}
